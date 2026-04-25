eb05dfbf4f32d38deb81ca7e3c63e32f9b82dca6
ppnaw4-crypto <pp.naw4@gmail.com>
2026-04-25 10:47:31 +0700

    Add files via upload

---
+++ cell02/cell02/ex02/password.py
@@ -0,0 +1,7 @@
+password = "Python is awesome"
+n = input()
+
+if n == password :
+    print("ACCESS GRANTED")
+else :
+    print("ACCESS DENIED")
---
+++ cell02/cell02/ex03/mult.py
@@ -0,0 +1,3 @@
+X = int(input("Enter the first number : "))
+Y = int(input("Enter the second number : "))
+print(X*Y)
