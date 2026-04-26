c716cbade31418bcd0e0370c32bcff107cc97b2e
ppnaw4-crypto <pp.naw4@gmail.com>
2026-04-26 15:31:13 +0000

    ok

--- cell04/cell04/ex03/float.py
+++ cell04/cell04/ex03/float.py
@@ -1,4 +1,11 @@
 X = input("Give me a number : ")
-A = int
-B = float
-if X == A 
+try:
+    num = float(X) 
+
+    if num.is_integer():
+        print("This number is an Integer.")
+    else:
+        print("This number is a Decimal.")
+
+except ValueError:
+    print("This is not a number.")
